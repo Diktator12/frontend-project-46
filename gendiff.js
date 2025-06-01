5e08c64dbdec0aa1e2d23d3b0c4f9535c862dc92
Diktator12 <tkachev.twilight@yandex.ru>
2025-06-01 15:02:41 +0300

    refactor: unify return type

--- gendiff.js
+++ gendiff.js
@@ -30,16 +30,22 @@ program
       const val1 = data1[key]
       const val2 = data2[key]
 
-      if (!_.has(data2, key)) return `  - ${key}: ${val1}`
-      if (!_.has(data1, key)) return `  + ${key}: ${val2}`
+      if (!_.has(data2, key)) {
+        return [`  - ${key}: ${val1}`]
+      }
+      if (!_.has(data1, key)) {
+        return [`  + ${key}: ${val2}`]
+      }
       if (!_.isEqual(val1, val2)) {
         return [`  - ${key}: ${val1}`,`  + ${key}: ${val2}`]
       }
 
-      return `    ${key}: ${val1}`
+      return [`    ${key}: ${val1}`]
     })
     
     return `{\n${diff.join('\n')}\n}`;
   }
 
   program.parse()
+
+export default genDiff
