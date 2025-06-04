27dc4ac54cee4fd1a023d80823d115707bd2e8d4
Diktator12 <tkachev.twilight@yandex.ru>
2025-06-04 20:46:24 +0300

    update gendiff CLI script

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,9 @@
 #!/usr/bin/env node
 import { Command } from "commander";
-import { readFileSync } from 'fs';
-import path from 'path';
-import genDiff from "../src/gendiff";
+import genDiff from "../src/gendiff.js";
+import { parseFile } from "../src/parsers.js";
 
 const program = new Command();
-
-const getFileData = (filepath) => {
-  const fullPath = path.resolve(process.cwd(), filepath)
-  return JSON.parse(readFileSync(fullPath, 'utf-8'))
-}
 
 program
   .name('gendiff')
@@ -18,10 +12,14 @@ program
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
   .action((filepath1, filepath2) => {
-    const data1 = getFileData(filepath1)
-    const data2 = getFileData(filepath2)
-    const result = genDiff(data1, data2)
-    console.log(result)
+    try {
+      const data1 = parseFile(filepath1)
+      const data2 = parseFile(filepath2)
+      const result = genDiff(data1, data2)
+      console.log(result)
+    } catch (err) {
+      console.error(err.message)
+    }
   })
 
 program.parse()
