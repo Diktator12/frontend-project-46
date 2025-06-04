cfd81060743e2f26716f9cf5c7a7ef01f6f448ac
Diktator12 <tkachev.twilight@yandex.ru>
2025-06-04 20:45:48 +0300

    update gendiff tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,19 +1,14 @@
-import { readFileSync } from 'fs';
-import path from 'path';
 import { fileURLToPath } from 'url';
+import path from 'path';
 import genDiff from '../src/gendiff.js';
+import { parseFile } from '../src/parsers.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const getFixturePath = (filename) => path.resolve(__dirname, '../__fixtures__', filename);
-const readJSON = (filename) => JSON.parse(readFileSync(getFixturePath(filename), 'utf-8'));
+const getFixturePath = (filename) => path.resolve(__dirname, '../__fixtures__', filename)
 
-test('gendiff flat JSON', () => {
-  const data1 = readJSON('file1.json');
-  const data2 = readJSON('file2.json');
-
-  const expected = `{
+const expected = `{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
@@ -22,5 +17,20 @@ test('gendiff flat JSON', () => {
   + verbose: true
 }`;
 
-  expect(genDiff(data1, data2)).toBe(expected);
+test('gendiff flat JSON', () => {
+  const data1 = parseFile(getFixturePath('JSON/file1.json'))
+  const data2 = parseFile(getFixturePath('JSON/file2.json'))
+
+  expect(genDiff(data1, data2)).toBe(expected)
+})
+
+test('gendiff flat YAML', () => {
+  const data1 = parseFile(getFixturePath('YAML/file1.yaml'))
+  const data2 = parseFile(getFixturePath('YAML/file2.yml'))
+
+  expect(genDiff(data1, data2)).toBe(expected)
+})
+
+test('throws error for unsupported file type', () => {
+  expect(() => parseFile(getFixturePath('./invalidFile.txt'))).toThrow('Unsupported file type: .txt');
 });
