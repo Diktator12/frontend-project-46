eed6208c23a65b3acfc1ca24e89e2d96d1e7a2f0
Diktator12 <tkachev.twilight@yandex.ru>
2025-06-05 19:17:45 +0300

    add format switcher

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,26 +1,15 @@
-import _ from "lodash";
+import buildDiff from "./buildDiff.js"
+import stylish from "./formatters/stylish.js"
 
-  const genDiff = (data1, data2) => {
-    const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)))
+const genDiff = (data1, data2, format = 'stylish') => {
+  const diff = buildDiff(data1, data2);
 
-    const diff = keys.flatMap((key) => {
-      const val1 = data1[key]
-      const val2 = data2[key]
-
-      if (!_.has(data2, key)) {
-        return [`  - ${key}: ${val1}`]
-      }
-      if (!_.has(data1, key)) {
-        return [`  + ${key}: ${val2}`]
-      }
-      if (!_.isEqual(val1, val2)) {
-        return [`  - ${key}: ${val1}`,`  + ${key}: ${val2}`]
-      }
-
-      return [`    ${key}: ${val1}`]
-    })
-    
-    return `{\n${diff.join('\n')}\n}`;
+  switch (format) {
+    case 'stylish':
+      return stylish(diff);
+    default:
+      throw new Error(`Unknown format: ${format}`);
   }
+};
 
-export default genDiff
+export default genDiff;
