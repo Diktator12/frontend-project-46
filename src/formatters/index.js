a3e1281dfd70ac58071213344fb409399b9b347f
Diktator12 <tkachev.twilight@yandex.ru>
2025-06-06 15:55:03 +0300

    add format selector

---
+++ src/formatters/index.js
@@ -0,0 +1,19 @@
+import stylish from './stylish.js'
+import plain from './plain.js'
+
+const formatters = {
+  stylish,
+  plain,
+}
+
+const format = (tree, formatName = 'stylish') => {
+  const formatter = formatters[formatName]
+
+  if (!formatter) {
+    throw new Error(`Unknown format: ${formatName}`)
+  }
+
+  return formatter(tree)
+}
+
+export default format
