24e265ea71e252827b3224e2a3b560a625351520
Diktator12 <tkachev.twilight@yandex.ru>
2025-06-06 22:09:16 +0300

    delete import and file parser

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander'
 import genDiff from '../src/gendiff.js'
-import { parseFile } from '../src/parsers.js'
 
 const program = new Command()
 
@@ -13,8 +12,6 @@ program
   .option('-f, --format [type]', 'output format')
   .action((filepath1, filepath2, options) => {
     try {
-      // const data1 = parseFile(filepath1)
-      // const data2 = parseFile(filepath2)
       const result = genDiff(filepath1, filepath2, options.format)
       console.log(result)
     } catch (err) {
