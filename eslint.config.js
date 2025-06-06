e6dd3b21225a28c1ecde9546e8a976214a86c633
Diktator12 <tkachev.twilight@yandex.ru>
2025-06-06 15:47:57 +0300

    update style and add semi rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-import js from "@eslint/js";
-import pkg from "globals";
-import pluginImport from "eslint-plugin-import";
-import pluginPrettier from "eslint-plugin-prettier";
-import prettierConfig from "eslint-config-prettier";
+import js from '@eslint/js'
+import pkg from 'globals'
+import pluginImport from 'eslint-plugin-import'
+import pluginPrettier from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
 
 const { node, jest } = pkg
 
@@ -12,13 +12,13 @@ export default [
   {
     languageOptions: {
       ecmaVersion: 2022,
-      sourceType: "module",
+      sourceType: 'module',
       globals: {
         ...node,
         ...jest
       },
       parserOptions: {
-        sourceType: "module"
+        sourceType: 'module'
       },
     },
     plugins: {
@@ -26,31 +26,33 @@ export default [
       prettier: pluginPrettier,
     },
     rules: {
-      "import/no-unresolved": "error",
-      "import/named": "error",
-      "import/default": "error",
-      "import/export": "error",
-      "import/no-named-as-default": "warn",
-      "import/no-named-as-default-member": "warn",
-      "import/no-extraneous-dependencies": [
-        "error",
+      'import/no-unresolved': 'error',
+      'import/named': 'error',
+      'import/default': 'error',
+      'import/export': 'error',
+      'import/no-named-as-default': 'warn',
+      'import/no-named-as-default-member': 'warn',
+      'import/no-extraneous-dependencies': [
+        'error',
         {
           devDependencies: [
-            "**/*.test.js",
-            "eslint.config.js",
+            '**/*.test.js',
+            'eslint.config.js',
           ],
         },
       ],
-      quotes: ["error", "single"],
-      "no-console": "off",
-      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      'no-console': 'off',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
       ...prettierConfig.rules,
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'never'],
+      'no-extra-semi': 'error',
     },
   },
   {
-    files: ["gendiff.js"],
+    files: ['gendiff.js'],
     rules: {
-      "import/no-extraneous-dependencies": "off",
+      'import/no-extraneous-dependencies': 'off',
     },
   },
-];
+]
