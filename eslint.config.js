179a625fecbd314342dbb2383a0fe000e8c1aca7
Diktator12 <tkachev.twilight@yandex.ru>
2025-06-07 00:07:38 +0300

    fix style

--- eslint.config.js
+++ eslint.config.js
@@ -50,7 +50,7 @@ export default [
       'semi': ['error', 'never'],
       'no-extra-semi': 'error',
 
-      '@stylistic/arrow-parens': ['error', 'as-needed', `${{ 'requireForBlockBody': true }}`],
+      '@stylistic/arrow-parens': ['error', 'as-needed', { requireForBlockBody: true }],
       '@stylistic/eol-last': ['error', 'always'],
       '@stylistic/indent': ['error', 2],
       '@stylistic/comma-dangle': ['error', 'always-multiline'],
