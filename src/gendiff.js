d3ef73136bbc1f5ef1df240634e8092cfda4286b
Diktator12 <tkachev.twilight@yandex.ru>
2025-06-06 20:13:23 +0300

    transfer logic of file parsing

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,7 +1,10 @@
 import buildDiff from './buildDiff.js'
 import format from './formatters/index.js'
+import { parseFile } from './parsers.js'
 
-const genDiff = (data1, data2, formatName = 'stylish') => {
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
+  const data1 = parseFile(filepath1)
+  const data2 = parseFile(filepath2)
   const diff = buildDiff(data1, data2)
   return format(diff, formatName)
 }
